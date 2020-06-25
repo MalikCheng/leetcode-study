bdaab0201d7d9b97dc5c3725f1177a560b443722
Malik <1484392767@qq.com>
2020-06-25 21:41:36 +0800

    finish 2.1

---
+++ Q2_1.java
@@ -0,0 +1,39 @@
+
+public class Q2_1 {
+
+    // size = k的子数组和的最大值
+   
+    // Input: [2, 1, 5, 1, 3, 2], k=3 
+    // Output: 9
+    // Explanation: Subarray with maximum sum is [5, 1, 3].
+    // Input: [2, 3, 4, 1, 5], k=2 
+    // Output: 7
+    // Explanation: Subarray with maximum sum is [3, 4].       
+    public static int findMaxSumSubArray(int k ,int arr[]){
+        if(arr == null) return -1;
+        int n = arr.length;
+        if(n<k||n==0) return -1;
+        int maxSum = 0;
+        int windowsRight = 0;
+        int windowsLeft = 0;
+        int windownSum = 0;
+        for(; windowsRight<n; windowsRight++){
+            windownSum += arr[windowsRight];
+            if(windowsRight>k-1){
+                windownSum -= arr[windowsLeft];
+                windowsLeft++;
+                maxSum = Math.max(maxSum,windownSum);
+            }
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+       int[] arr = {2, 1, 5, 1, 3, 2};
+       int k = 3;
+       System.out.println(findMaxSumSubArray(k, arr));
+        
+       arr = new int[]{2, 3, 4, 1, 5}; 
+       k=2;
+       System.out.println(findMaxSumSubArray(k, arr));
+    }
+}
